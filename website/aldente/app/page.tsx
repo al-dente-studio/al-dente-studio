bd4cca3a7e6e87389eb65efd9812707b08d41700
kkratterf <kkratterf@gmail.com>
2024-01-09 10:21:56 +0100

    Removed Spaghetto Design System

---
+++ website/aldente/app/page.tsx
@@ -0,0 +1,67 @@
+export default function Home() {
+  return (
+    <main className="max-w-screen-2xl mx-auto">
+      <div className="grid grid-cols-12 gap-0 w-full divide-y divide-[#BC5974]">
+        <div className="w-full aspect-square grid grid-cols-subgrid col-span-12 divide-x divide-[#BC5974]">
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+        </div>
+        <div className="w-full aspect-square grid col-span-4 p-6 text-3xl font-bold text-[#EDEDED]">
+          the design system of tomorrow
+        </div>
+        <div className="grid grid-cols-subgrid w-full aspect-square divide-y divide-[#BC5974]">
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+          <div className="w-full aspect-square grid"></div>
+        </div>
+        <div className="w-full aspect-square grid col-span-4"></div>
+        <div className="w-full aspect-square grid col-span-2"></div>
+      </div>
+    </main>
+  );
+}
+
+/*
+<div className="text-8xl font-medium">
+        In questo scorcio di tempo che appartiene alla nostra età, il Novecento,
+        ci rivolgiamo a un piatto che incarna la tradizione e l&apos;innovazione
+        nella cucina italiana: la carbonara. Una pietanza che, nel suo
+        complesso, è una sinfonia di sapori e texture, che parla direttamente
+        all&apos;anima di chi la gusta. La Selezione degli Ingredienti: Il cuore
+        di una buona carbonara risiede nella qualità dei suoi componenti. Un
+        uovo fresco, la pancetta o guanciale robusto, pecorino romano
+        grattugiato fino e la pasta, preferibilmente spaghetti o rigatoni, che
+        deve essere di eccellente fattura. Il Ritmo della Cottura: L&apos;acqua
+        bolle in una pentola, attendendo i fili d&apos;oro della pasta. Questa
+        deve essere cotta al punto giusto, &apos;al dente&apos;, mantenendo una
+        certa resistenza sotto il morso. Nel frattempo, il guanciale viene
+        rosolato in una padella fino a quando non diventa croccante, ma non
+        bruciato. La Composizione della Salsa: In una ciotola, si mescolano le
+        uova con abbondante pecorino e pepe nero macinato fresco. Questa unione,
+        semplice ma potente, è il segreto di una carbonara sublime.
+        L&apos;Assemblaggio: Una volta che la pasta è cotta, viene trasferita
+        nella padella con il guanciale, mescolando per far assorbire i grassi.
+        Poi, lontano dal fuoco, si aggiunge la miscela di uova e formaggio,
+        mescolando rapidamente per creare una crema setosa che avvolge ogni
+        singolo filo di pasta. Il Rito Finale: La carbonara è servita calda, con
+        un&apos;ulteriore spolverata di pecorino e un pizzico di pepe nero. Ogni
+        boccone è un viaggio attraverso secoli di tradizione culinaria,
+        un&apos;opera d&apos;arte che si dispiega nel palato. In questo trattato
+        abbiamo tracciato non solo la strada per creare una carbonara perfetta,
+        ma abbiamo anche esplorato il legame profondo tra la cucina e la cultura
+        italiana. La carbonara non è semplicemente un piatto, ma un simbolo
+        della storia e dell&apos;identità di un popolo che ha saputo trasformare
+        l&apos;arte della cucina in un linguaggio universale.
+      </div>
+*/
